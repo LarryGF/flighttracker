41b21dbd365c1a8c786fd5ae72b61ca818432e9f
LarryGF <larrywtf609@gmail.co>
2019-08-05 19:16:17 -0400

    Initial Commit

---
+++ downloadscript.py
@@ -0,0 +1,98 @@
+import requests
+import click
+import json
+
+def get_info(airport, date, time):
+    url = f'https://www.flightstats.com/v2/api-next/flight-tracker/dep/{airport}/{date}/{time}'
+
+    try:
+        r = requests.get(url)
+        return r.json()
+
+    except ConnectionError:
+        print('2- site not reachable')
+        exit(2)
+
+    except:
+        print('0 - fail')
+        exit(0)
+
+
+@click.command()
+def help():
+    print("""
+    usage:
+"airport name" "date" "time" "output_option"
+
+airport name mean airport code for example HAV for havana international airport
+
+date in format (year/month/day)
+
+"time"
+1. 6AM-12PM
+2. 12PM - 6PM
+3. 6pm - 12AM
+4. 12AM-6AM
+5. All
+
+output option:
+1 - standard output
+2 - file json
+3 - standard output then file json
+
+Return function
+0 - fail
+1 - success
+2- site not reachable
+3 - no output
+
+
+    """
+          )
+
+
+@click.command()
+@click.option('--airport_name', default='HAV', help='airport name mean airport code for example HAV for havana international airport')
+@click.option('--date', default='2019/08/6', help='date in format year/month/day')
+@click.option('--time', default= 1, help="""
+"time"
+1. 6AM-12PM
+2. 12PM - 6PM
+3. 6pm - 12AM
+4. 12AM-6AM
+5. All
+""")
+
+@click.option('--output_option', default= 1, help="""
+output option:
+1 - standard output
+2 - file json
+3 - standard output then file json
+""")
+def extract(airport_name, date, time, output_option):
+
+    ans = []
+
+    if time < 5:
+        data = get_info(airport_name, date, (time % 4) * 6)
+
+    else:
+        data = [get_info(airport_name, date, (time % 4) * 6) for i in range(1,5)]
+
+    data = json.dumps(data, ensure_ascii=False, indent=2)
+    if output_option == 1:
+        print(data)
+
+    if output_option == 2:
+        open('data.json','w').write(data)
+        exit(3)
+
+
+    if output_option == 3:
+        print(data)
+        open('data.json','w').write(data)
+
+    exit(1)
+
+if __name__=='__main__':
+    extract()
